e2673f52b5f52275cafaad0ce13079c0d4d1bf2f
wikanda <noo_lek_29@hotmail.com>
2012-10-24 11:07:41 +0700

    555

--- Agile/Bomb/src/agile/Player.java
+++ Agile/Bomb/src/agile/Player.java
@@ -10,4 +10,26 @@ package agile;
  */
 public class Player {
     
+    private int playerNumber;
+    private boolean turn;
+    
+    public Player(){
+        
+    }
+    
+    public boolean getTurn(){
+        return this.turn;
+    }
+    
+    public void setTurn(boolean turn){
+        this.turn = turn;
+    }
+    
+    public int getPlayerNumber(){
+        return this.playerNumber;
+    }
+    
+    public void setPlayerNumber(int number){
+        this.playerNumber = number;
+    }
 }
